d4baa9e900993724c0c77045120dc9bdd39e6175
seo17 <ebenmosi17@gmail.com>
2024-07-08 23:50:00 +0100

    UI Design Added

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home, Login, SignUp } from "../pages";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+export default router;
