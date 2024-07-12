cf9c0732b3e5a1fe15a5a8461a3682f6a82e76b4
seo17 <ebenmosi17@gmail.com>
2024-07-12 23:57:54 +0100

    fix deployment bug

---
+++ backend/dist/src/controllers/auth.controller.js
@@ -0,0 +1,102 @@
+import prisma from "../db/prisma.js";
+import bcryptjs from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+export const signup = async (req, res) => {
+    try {
+        const { fullName, username, password, confirmPassword, gender } = req.body;
+        if (!fullName || !username || !password || !confirmPassword || !gender) {
+            res.status(400).json({ error: "Please fill all fields" });
+        }
+        if (password !== confirmPassword) {
+            res.status(400).json({ error: "Passwords don't match" });
+        }
+        const user = await prisma.user.findUnique({ where: { username } });
+        if (user) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+        // Protect Password
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+        // Generate Random profile picture
+        const boyProfilePic = `https://avatar-placeholder.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar-placeholder.iran.liara.run/public/girl?username=${username}`;
+        const newUser = await prisma.user.create({
+            data: {
+                fullName,
+                username,
+                password: hashedPassword,
+                gender,
+                profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+            },
+        });
+        if (newUser) {
+            // generate token
+            generateToken(newUser.id, res);
+            res.status(201).json({
+                id: newUser.id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        }
+        else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+    }
+    catch (error) {
+        console.log("Error in sigup controller", error.message);
+        res.status(500).json({ error: "Interal Server Error" });
+    }
+};
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({ where: { username } });
+        if (!user) {
+            return res.status(400).json({ error: "Invalid credentials" });
+        }
+        const isPasswordCorrect = await bcryptjs.compare(password, user?.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ error: "Invalid credentials" });
+        }
+        generateToken(user.id, res);
+        res.status(200).json({
+            id: user.id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    }
+    catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Interal Server Error" });
+    }
+};
+export const logout = async (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged Out successfully" });
+    }
+    catch (error) {
+        console.log("Error in Logout controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+export const getMe = async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({ where: { id: req?.user.id } });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json({
+            id: user.id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    }
+    catch (error) {
+        console.log("Error in getMe controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
